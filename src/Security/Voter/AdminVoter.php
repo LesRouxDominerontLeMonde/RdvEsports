67bffb55cacb8f331c4092dc7685124ae5478f5a
LesRouxDominerontLeMonde <LesRouxDominerontLeMonde@gmail.com>
2025-02-22 17:31:30 +0100

    AdminVoter implementation

--- src/Security/Voter/AdminVoter.php
+++ src/Security/Voter/AdminVoter.php
@@ -8,15 +8,9 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 final class AdminVoter extends Voter
 {
-    public const EDIT = 'POST_EDIT';
-    public const VIEW = 'POST_VIEW';
-
     protected function supports(string $attribute, mixed $subject): bool
     {
-        // replace with your own logic
-        // https://symfony.com/doc/current/security/voters.html
-        return in_array($attribute, [self::EDIT, self::VIEW])
-            && $subject instanceof \App\Entity\Admin;
+        return true;
     }
 
     protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool
@@ -28,19 +22,6 @@ final class AdminVoter extends Voter
             return false;
         }
 
-        // ... (check conditions and return true to grant permission) ...
-        switch ($attribute) {
-            case self::EDIT:
-                // logic to determine if the user can EDIT
-                // return true or false
-                break;
-
-            case self::VIEW:
-                // logic to determine if the user can VIEW
-                // return true or false
-                break;
-        }
-
-        return false;
+        return in_array('ROLE_ADMIN', $user->getRoles());
     }
 }
